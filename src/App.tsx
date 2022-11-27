349ec4d048a30d0ac22494d1532ac1389e458e4f
AndreyGalchevski <andreygalchevski@gmail.com>
2022-11-27 18:24:42 +0200

    move all the app-related context providers to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { Container, styled } from "@mui/material";
+import { Container, CssBaseline, styled } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import SystemModal from "./components/SystemModal";
 import UploadCard from "./components/UploadCard";
+import { ModalProvider } from "./context/ModalContext";
 
 const CenteredContainer = styled(Container)({
   display: "flex",
@@ -11,13 +13,30 @@ const CenteredContainer = styled(Container)({
   height: "100vh",
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <CenteredContainer>
-        <UploadCard />
-      </CenteredContainer>
-      <SystemModal />
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <ModalProvider>
+          <CenteredContainer>
+            <UploadCard />
+          </CenteredContainer>
+          <SystemModal />
+        </ModalProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CssBaseline } from "@mui/material";
-import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App";
-import { ModalProvider } from "./context/ModalContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      cacheTime: Infinity,
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-      retry: false,
-    },
-  },
-});
-
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <QueryClientProvider client={queryClient}>
-      <ModalProvider>
-        <App />
-      </ModalProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
